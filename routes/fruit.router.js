c6e11e35c70944d5bc296dc95e8fc55c6b95c73a
bruno-fonoff <105615381+bruno-fonoff@users.noreply.github.com>
2022-07-13 20:00:34 -0300

    :white_check_mark: CRRUD OK

--- routes/fruit.router.js
+++ routes/fruit.router.js
@@ -40,4 +40,34 @@ router.get("/:id", async (req, res) => {
 
 //UPDATE
 
+router.patch("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const editFruit = await FruitModel.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true }
+    );
+    return res.status(200).json(editFruit);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
+
+//DELETE
+
+router.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteFruit = await FruitModel.deleteOne({ _id: id });
+    return res.status(200).json(deleteFruit);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
+
 module.exports = router;
